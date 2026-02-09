eb57dce41741c1f532b20433f4fc537e8933ec90
EsBazza <alonzoamarojuno@gmail.com>
2026-02-09 22:35:49 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function Header({ name }) {
+  return <h1 className="header">{name}</h1>
+}
+function Box({ subject }) {
+  return (
+    <div className="box">
+      {subject}
+    </div>
+  )
+}
+export default function App() {
+  const name = "Amaro Juno Alonzo";
+  const subject = "CPEITEL";
+  return (
+    <div className="app">
+      <Header name={name} />
+      <Box subject={subject} />
+    </div>
+  )
+}
+
